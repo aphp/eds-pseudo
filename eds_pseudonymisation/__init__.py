77e81870710228ab449f3401f8781ab6770c671a
Perceval Wajsbürt <perceval.wajsburt-ext@aphp.fr>
2022-12-09 13:39:36 +0100

    feat: fix tokenization issues by change lang from spacy's "fr" to edsnlp's "eds"

--- eds_pseudonymisation/__init__.py
+++ eds_pseudonymisation/__init__.py
@@ -1,3 +1,10 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
+
+from . import evaluate
+from .pipelines.addresses import PseudonymisationAddresses
+from .pipelines.dates import PseudonymisationDates
+from .pipelines.pseudonymisation import Pseudonymisation
+from .pipelines.clean_entities import CleanEntities
+from .pipelines.structured import StructuredDataMatcher
