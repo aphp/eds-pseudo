2cac88050b56109f61c229f7455b560b0f37e9ee
Perceval Wajsbürt <perceval.wajsburt-ext@aphp.fr>
2022-12-09 10:58:35 +0100

    feat: improve rule-based date detection

--- eds_pseudonymisation/pipelines/dates/dates.py
+++ eds_pseudonymisation/pipelines/dates/dates.py
@@ -1,6 +1,7 @@
+import re
 from typing import Dict, List, Optional, Tuple
 
-from edsnlp.pipelines.misc.dates import Dates
+from edsnlp.pipelines.misc.dates.dates import Dates
 from edsnlp.pipelines.misc.dates.models import AbsoluteDate
 from edsnlp.utils.filter import filter_spans
 from pydantic import BaseModel, root_validator
@@ -9,16 +10,15 @@ from spacy.tokens import Doc, Span
 if not Span.has_extension("date_string"):
     Span.set_extension("date_string", default=None)
 
-
-class DateString(BaseModel):
-    day: Optional[str] = None
-    month: Optional[str] = None
-    year: Optional[str] = None
-
-    @root_validator(pre=True)
-    def normalise_keys(cls, d: Dict[str, Optional[str]]) -> Dict[str, str]:
-        normalised = {k.split("_")[0]: v for k, v in d.items() if v is not None}
-        return normalised
+# class DateString(BaseModel):
+#     day: Optional[str] = None
+#     month: Optional[str] = None
+#     year: Optional[str] = None
+#
+#     @root_validator(pre=True)
+#     def normalise_keys(cls, d: Dict[str, Optional[str]]) -> Dict[str, str]:
+#         normalised = {k.split("_")[0]: v for k, v in d.items() if v is not None}
+#         return normalised
 
 
 class PseudonymisationDates(Dates):
@@ -45,7 +45,7 @@ class PseudonymisationDates(Dates):
                 parsed = AbsoluteDate.parse_obj(groupdict)
 
                 span._.date = parsed
-                span._.date_string = DateString.parse_obj(groupdict)
+                # span._.date_string = DateString.parse_obj(groupdict)
 
                 spans.append(span)
 
@@ -65,10 +65,20 @@ class PseudonymisationDates(Dates):
         doc : Doc
             spaCy Doc object, annotated for dates
         """
-        dates = self.process(doc)
-        dates = self.parse(dates)
+        dates = []
+        birth_date = None
+        for date in self.parse(self.process(doc)):
+            snippet = doc[max(0, date.start - 5) : date.end + 5].text
+            if re.search(r"\b(né|n[ée]e|naissance)\b", snippet, flags=re.IGNORECASE):
+                date.label_ = "DATE_NAISSANCE"
+                birth_date = date._.date
+            else:
+                date.label_ = "DATE"
+            dates.append(date)
 
-        dates = [Span(doc, d.start, d.end, label="DATE") for d in dates]
+        for date in dates:
+            if date._.date == birth_date:
+                date.label_ = "DATE_NAISSANCE"
 
         ents = filter_spans(list(doc.ents) + dates, return_discarded=False)
 
