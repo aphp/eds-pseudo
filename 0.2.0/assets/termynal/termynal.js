ab80d72d2c97cbf3106ec35e3a81a2e0f1852e23
Perceval Wajsbürt <perceval.wajsburt-ext@aphp.fr>
2023-04-06 11:17:27 +0100

    Deployed c53b0cc to 0.2.0 with MkDocs 1.4.2 and mike 1.1.2

--- 0.2.0/assets/termynal/termynal.js
+++ 0.2.0/assets/termynal/termynal.js
@@ -330,11 +330,12 @@ function createTermynals() {
                     if (isBlankSpace) {
                         dataValue["delay"] = 0;
                     }
-                    if (buffer[buffer.length - 1] === "") {
+                    /*if (buffer[buffer.length - 1] === "") {
                         // A last single <br> won't have effect
                         // so put an additional one
                         buffer.push("");
-                    }
+                    }*/
+
 
                     const bufferValue = buffer.join("<br>");
                     dataValue["value"] = bufferValue;
